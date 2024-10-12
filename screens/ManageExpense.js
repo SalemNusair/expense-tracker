3e7b418999375dae8248476b22cb9ce6b8db3433
SalemNusair <52820565+SalemNusair@users.noreply.github.com>
2024-10-12 12:18:19 +0300

    feat(wip): manage expense

---
+++ screens/ManageExpense.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+const ManageExpense = () => {
+    return (
+        <View>
+            <Text>Manage Expense Screen</Text>
+        </View>
+    );
+};
+export default ManageExpense;
