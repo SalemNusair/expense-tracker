43d3ecc048cad75c895854b88c9a634610c085c4
SalemNusair <52820565+SalemNusair@users.noreply.github.com>
2024-10-12 12:17:25 +0300

    feat: expenses output

---
+++ screens/AllExpenses.js
@@ -0,0 +1,6 @@
+import { Text, View } from "react-native";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+const AllExpenses = () => {
+    return <ExpensesOutput expensesPeriod={"total"} />;
+};
+export default AllExpenses;
