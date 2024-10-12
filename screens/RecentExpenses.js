7915c871d41a074e0ab6be85ff4df2e6805a69d0
SalemNusair <52820565+SalemNusair@users.noreply.github.com>
2024-10-12 12:17:50 +0300

    feat: recent expenses

---
+++ screens/RecentExpenses.js
@@ -0,0 +1,6 @@
+import { Text, View } from "react-native";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+const RecentExpenses = () => {
+    return <ExpensesOutput expensesPeriod={"Last 7 Days"} />;
+};
+export default RecentExpenses;
